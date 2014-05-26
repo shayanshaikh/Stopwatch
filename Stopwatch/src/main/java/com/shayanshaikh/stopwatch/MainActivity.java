8eb6b57f088c35200b686dc4232dbc21e5eb2c64
Shayan Shaikh <shayanshaikh2@gmail.com>
2014-05-26 14:29:13 -0700

    Step Two

--- Stopwatch/src/main/java/com/shayanshaikh/stopwatch/MainActivity.java
+++ Stopwatch/src/main/java/com/shayanshaikh/stopwatch/MainActivity.java
@@ -14,23 +14,25 @@ public class MainActivity extends ActionBarActivity {
 
     private Button start;
     private Button stop;
+    private Button reset;
     private Chronometer MyChronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hideStopButton();
         uI();
     }
 
     public void uI() {
         MyChronometer = (Chronometer) findViewById(R.id.chronometer);
-        MyChronometer.setBase(SystemClock.elapsedRealtime());
         start = (Button) findViewById(R.id.btstart);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MyChronometer.start();
+                showStopButton();
             }
         });
         stop = (Button) findViewById(R.id.btstop);
@@ -38,8 +40,28 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
                 MyChronometer.stop();
+                hideStopButton();
             }
         });
+        reset = (Button) findViewById(R.id.btreset);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyChronometer.setBase(SystemClock.elapsedRealtime());
+            }
+        });
+    }
+
+    private void showStopButton() {
+        start.setVisibility(View.GONE);
+        reset.setVisibility(View.GONE);
+        stop.setVisibility(View.VISIBLE);
+    }
+
+    private void hideStopButton() {
+        start.setVisibility(View.VISIBLE);
+        reset.setVisibility(View.VISIBLE);
+        stop.setVisibility(View.GONE);
     }
 
     @Override
